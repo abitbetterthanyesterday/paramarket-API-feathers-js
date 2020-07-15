a51d851373e7314b7e03492e47b9a6894dfe703b
Aloys Berger <aloysberger@gmail.com>
2020-07-15 17:14:40 +0200

    Set up ENV and DB connection

--- src/knex.js
+++ src/knex.js
@@ -1,7 +1,9 @@
 const knex = require('knex');
 
 module.exports = function (app) {
-  const { client, connection } = app.get('mysql');
+  // const { client, connection } = app.get('mysql');
+  client = "mysql2",
+  connection = `mysql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:3306/API_paramarket`
   const db = knex({ client, connection });
 
   app.set('knexClient', db);
